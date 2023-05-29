daf080b17eff2ce3e212a2209e15048ccdb24fb6
Lightning8 <tov.kessler@yandex.ru>
2023-05-30 02:29:03 +0300

    fix: app port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     bufferLogs: true,
   });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
