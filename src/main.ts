440e3cdc6d6957d010cb67709d7ab3baf6cab8be
Lightning8 <tov.kessler@yandex.ru>
2023-07-05 21:37:55 +0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  app.enableCors();
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   await app.listen(3001);
 }
